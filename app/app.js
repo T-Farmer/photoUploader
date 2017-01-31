3513f87ba4bd42bd4fcbe7989d163882fdbc1847
T-Farmer <terrill@Rels-Pro-186.local>
2017-01-31 10:30:28 -0600

    setup

---
+++ app/app.js
@@ -0,0 +1,13 @@
+console.log("Hi")
+
+const app = angular.module('photoApp', ['ngRoute'])
+
+app.config(($routeProvider, $locationProvider) => {
+  $locationProvider.hashPrefix('')
+  $routeProvider
+      .when('/', {
+        controller: 'HomeCtrl',
+        templateUrl: 'partials/home.html',
+      })
+
+})
